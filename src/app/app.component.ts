40cdca2f14460cfb7da24c1c76537e178cc5d035
Fer <fernando.flores.flores.3f@gmail.com>
2022-11-16 05:44:42 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
+import { environment } from './../environments/environment';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
